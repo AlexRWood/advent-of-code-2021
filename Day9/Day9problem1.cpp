fee9c889fd7ca590eff032cc0bd4ad8d2afd059e
Alex Wood <alexryanwood@icloud.com>
2021-12-14 19:12:35 -0400

    Finished Day 9 problem 1

--- Day9/Day9problem1.cpp
+++ Day9/Day9problem1.cpp
@@ -10,6 +10,7 @@ void checkInner(std::vector<std::vector<int>> &v, int &score);
 
 int main() {
     std::cout << solution("test.txt") << std::endl;
+    std::cout << solution("input.txt") << std::endl;
 }
 
 int solution(std::string s) {
@@ -17,6 +18,7 @@ int solution(std::string s) {
     readFile(s, heightMap);
     int score {0};
     checkEdges(heightMap, score);
+    checkInner(heightMap, score);
     return score;
 }
 
@@ -55,17 +57,62 @@ void checkEdges(std::vector<std::vector<int>> &v, int &score) {
    bool smallerThanUp {false};
    bool smallerThanDown {false};
 
-    for(int32_t i {1}; i < rightmostColumn; i++) {
-        smallerThanDown = v[topRow][i] < v[topRow+1][i];
-        smallerThanLeft = v[topRow][i] < v[topRow][i-1];
-        smallerThanRight = v[topRow][i] < v[topRow][i+1];
+    for(int i {1}; i < rightmostColumn; i++) {
+        const int height = v[topRow][i];
+        smallerThanDown = height < v[topRow+1][i];
+        smallerThanLeft = height < v[topRow][i-1];
+        smallerThanRight = height < v[topRow][i+1];
         if(smallerThanDown && smallerThanLeft && smallerThanRight) {
-            score += (v[topRow][i] + 1);
+            score += (height + 1);
         }
     }
+    for(int i {1}; i < bottomRow; i++) {
+        const int height = v[i][leftmostColumn];
+        smallerThanDown = height < v[i + 1][leftmostColumn];
+        smallerThanRight = height < v[i][leftmostColumn + 1];
+        smallerThanUp = height < v[i - 1][leftmostColumn];
+        if(smallerThanDown && smallerThanRight && smallerThanUp) {
+            score += (height + 1);
+        }
+    }
+    for(int i {1}; i < rightmostColumn; i++) {
+        const int height = v[bottomRow][i];
+        smallerThanLeft = height < v[bottomRow][i - 1];
+        smallerThanRight = height < v[bottomRow][i + 1];
+        smallerThanUp = height < v[bottomRow - 1][i];
+        if(smallerThanLeft && smallerThanRight && smallerThanUp) {
+            score += (height + 1);
+        }
+    }
+    for(int i {1}; i < bottomRow; i++) {
+        const int height = v[i][rightmostColumn];
+        smallerThanLeft = height < v[i][rightmostColumn - 1];
+        smallerThanDown = height < v[i + 1][rightmostColumn];
+        smallerThanUp = height < v[i - 1][rightmostColumn];
+        if(smallerThanLeft && smallerThanDown && smallerThanUp) {
+            score += (height + 1);
+        }
+    }
+
+
 
 }
 
 void checkInner(std::vector<std::vector<int>> &v, int &score) {
-    //TODO
+    bool smallerThanRight {false};
+    bool smallerThanLeft {false};
+    bool smallerThanUp {false};
+    bool smallerThanDown {false};
+    for(int i {1}; i < v.size() - 1; i++) {
+        for(int j {1}; j < v[i].size() - 1; j++) {
+            const int height {v[i][j]};
+            smallerThanDown = height < v[i + 1][j];
+            smallerThanUp = height < v[i - 1][j];
+            smallerThanLeft = height < v[i][j - 1];
+            smallerThanRight = height < v[i][j + 1];
+            if(smallerThanDown && smallerThanLeft && smallerThanRight && smallerThanUp) {
+                score += (height + 1);
+            }
+        }
+    }
 }
